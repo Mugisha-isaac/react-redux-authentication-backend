4a457fd4e61198b796af59db477c72aef1d4367c
Mugisha-isaac <mugishaisaac2001@gmail>
2022-02-23 08:05:20 +0200

    models

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
-    res.json('welcome on react redux authentication course');
+    res.json({message:'welcome on react redux authentication course'});
 })
 
 const port = process.env.PORT || 8080;
